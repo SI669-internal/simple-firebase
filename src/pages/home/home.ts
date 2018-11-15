acc4b7d8e2e3e1d574a9bfc1f3586e321bf2f636
Newman <mwnewman@0587394227.wireless.umich.net>
2018-11-15 09:53:11 -0500

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import firebase from 'firebase';
+
+const firebaseConfig  = {
+  apiKey: "AIzaSyDijqNcYsDxLwDT9JDR1gctn1m99ZZCA4w",
+  authDomain: "week10-si699-mwnewman.firebaseapp.com",
+  databaseURL: "https://week10-si699-mwnewman.firebaseio.com",
+  projectId: "week10-si699-mwnewman",
+  storageBucket: "week10-si699-mwnewman.appspot.com",
+  messagingSenderId: "304295505344"
+};
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +18,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  private db: any;
+  private item: string = "";
+
   constructor(public navCtrl: NavController) {
+    firebase.initializeApp(firebaseConfig);
+    this.db = firebase.database();
+    // this.appDataRef.on('value', snapshot => {
+    //   console.log(snapshot.toJSON());
+    // });
 
   }
+  private saveToFirebase() {
+    let dataRef = this.db.ref('/');
+    dataRef.set({itemKey: this.item});
+  }
+
 
 }
